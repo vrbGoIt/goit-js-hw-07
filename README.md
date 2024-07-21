a741075f491eac234673efdcffaa664f48df317b
Roman <46716068+vrbprog@users.noreply.github.com>
2024-07-21 15:16:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# goit-js-hw-03
+
+JS HomeWork3
---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function slugify(title) {
+    const mess = title.toLowerCase().split(" ").join("-");
+    return mess;
+}
+
+console.log("\n**********  Task-1  ************");
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
