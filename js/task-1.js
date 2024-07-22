21cff525b6a56d8825adee9d6786aa1f702988a2
Roman <vrbox@i.ua>
2024-07-22 10:50:56 +0300

    Done Task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 const list = document.querySelectorAll('#categories > .item');
-console.log(list);
+// console.log(list);
 console.log(`Number of categories: ${list.length}`);
 list.forEach(element => {
     console.log('Category: ' + element.querySelector('h2').textContent);
