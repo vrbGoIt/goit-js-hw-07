9bee1bd6f2d33b005511010aceae335a5c800b02
Roman <vrbox@i.ua>
2024-09-13 10:02:06 +0300

    Done Task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
 
+const form = document.querySelector(".login-form");
+const user = {};
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    user.email = event.target.elements.email.value.trim();
+    user.password = event.target.elements.password.value.trim();
+
+    if (user.email === "" || user.password === "") {
+        alert('All form fields must be filled in');
+    }
+    else {
+        form.reset();
+        console.log(user);
+    }
+});
+
+
