b9424e5baba9c91274beee9751c203e81ed753c3
Roman <vrbox@i.ua>
2024-09-13 13:05:10 +0300

    Done Task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,28 @@
+
+const button = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const span = document.querySelector("span.color");
+
+button.addEventListener("click", event => {
+  const hex = getRandomHexColor();
+  body.style.backgroundColor = "rgb(" +
+    parseInt(hex.slice(1, 3), 16) + ',' +
+    parseInt(hex.slice(3, 5), 16) + ',' +
+    parseInt(hex.slice(5, 7), 16) + ")";
+  ;
+  
+  span.style.color = "rgb(" +
+    parseInt(hex.slice(1, 3), 16) + ',' +
+    parseInt(hex.slice(3, 5), 16) + ',' +
+    parseInt(hex.slice(5, 7), 16) + ")";
+  ;
+
+});
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
