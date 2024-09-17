1ae62ae1dfcb74deb72d30be8fb05a87b8d6dbf1
Roman <vrbox@i.ua>
2024-09-17 09:55:09 +0300

    Done Task5

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,36 @@
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+const box = document.querySelector("div#boxes");
+const numboxes = document.querySelector("div#controls > input");
+
+let size = 30;
+let boxis = '';
+let boxstr = '';
+
+
+buttonCreate.addEventListener("click", event => {
+
+  if (numboxes.value > 0 && numboxes.value <= 100) {
+    const numb = numboxes.value;
+    box.style.padding = "32px";
+    for (let index = 0, size = 30; index < numb; index++) {
+      let color = getRandomHexColor();
+      boxstr = `<div style="background-color: ${color}; max-width: ${size}px; max-height: ${size}px; min-width: ${size}px; min-height: ${size}px;"></div>`;
+      boxis = boxis.concat(boxstr);
+      size += 10;
+    }
+
+    box.insertAdjacentHTML("beforeend", boxis);
+  }
+});
+
+buttonDestroy.addEventListener("click", event => {
+    box.style.padding = "0px";
+    box.innerHTML = '';
+    boxis = '';
+});  
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
